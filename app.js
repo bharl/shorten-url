899772c7b19b04599a322d1a42cfd68b83029a8c
Lucas Herrera <lucas.herrera@hotmail.com>
2018-04-18 22:08:32 -0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const express    = require('express');
+const app 	     = express();
+
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', route);
+
+app.get('/', (req, res) => {
+	res.status(200).end();
+});
+
+app.listen(port, () => {
+	console.log('Listening on port' + port + '...');
+});
